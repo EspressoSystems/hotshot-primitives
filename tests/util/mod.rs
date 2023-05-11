69c218095dc78b02bfc2c983d0265142e2d76ac1
Gus Gutoski <ggutoski@users.noreply.github.com>
2023-05-11 17:30:10 -0400

    move round_trip into util/mod.rs

--- tests/util/mod.rs
+++ tests/util/mod.rs
@@ -1,41 +1,12 @@
-use hotshot_primitives::vid::{advz::Advz, VidScheme};
+use hotshot_primitives::vid::VidScheme;
 
-use ark_bls12_381::Bls12_381;
-use ark_ec::pairing::Pairing;
-use ark_ff::{Field, PrimeField};
 use ark_std::{
     println,
     rand::{seq::SliceRandom, CryptoRng, RngCore},
     vec,
 };
-use jf_primitives::pcs::{prelude::UnivariateKzgPCS, PolynomialCommitmentScheme};
-use jf_utils::test_rng;
-use sha2::Sha256;
 
-type Pcs = UnivariateKzgPCS<Bls12_381>;
-type G = <Bls12_381 as Pairing>::G1Affine;
-type H = Sha256;
-
-#[test]
-fn round_trip_advz() {
-    let mut rng = test_rng();
-    let srs = Pcs::gen_srs_for_testing(&mut rng, 3).unwrap();
-
-    println!(
-            "modulus byte len: {}",
-            (<<Pcs as PolynomialCommitmentScheme>::Evaluation as Field>::BasePrimeField
-                ::MODULUS_BIT_SIZE - 7)/8 + 1
-        );
-
-    round_trip(
-        |payload_chunk_size, num_storage_nodes| {
-            Advz::<Pcs, G, H>::new(payload_chunk_size, num_storage_nodes, &srs).unwrap()
-        },
-        &mut rng,
-    );
-}
-
-fn round_trip<V, R>(vid_factory: impl Fn(usize, usize) -> V, rng: &mut R)
+pub fn round_trip<V, R>(vid_factory: impl Fn(usize, usize) -> V, rng: &mut R)
 where
     V: VidScheme,
     R: RngCore + CryptoRng,
