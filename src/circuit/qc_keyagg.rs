180711ca0100d4843891cc92813dd714cbb6a9b2
Charles Chen <chancharles92@gmail.com>
2023-05-25 10:38:29 -0400

    draft circuits for keyagg and digest_hash

--- src/circuit/qc_keyagg.rs
+++ src/circuit/qc_keyagg.rs
@@ -1,7 +1,8 @@
 //! Circuit implementation of stake key aggregation for quorum certificates.
 
+use ark_ec::twisted_edwards::TECurveConfig as Config;
 use ark_std::{format, vec};
-use jf_primitives::rescue::RescueParameter;
+use jf_primitives::{circuit::rescue::RescueNativeGadget, rescue::RescueParameter};
 use jf_relation::{
     errors::CircuitError, gadgets::ecc::PointVariable, BoolVar, Circuit, PlonkCircuit, Variable,
 };
@@ -12,9 +13,10 @@ use jf_relation::{
 pub struct VerKeyVar(pub PointVariable);
 
 /// Plonk circuit gadget for stake key aggregation for quorum certificates.
-pub trait QCKeyAggregateGadget<F>
+pub trait QCKeyAggregateGadget<F, P>
 where
     F: RescueParameter,
+    P: Config<BaseField = F>,
 {
     /// Key aggregation circuit
     /// * `vks` - list of stake public keys.
@@ -50,26 +52,56 @@ where
     ) -> Result<(), CircuitError>;
 }
 
-impl<F> QCKeyAggregateGadget<F> for PlonkCircuit<F>
+impl<F, P> QCKeyAggregateGadget<F, P> for PlonkCircuit<F>
 where
     F: RescueParameter,
+    P: Config<BaseField = F>,
 {
     fn check_aggregate_vk(
         &mut self,
-        _vks: &[VerKeyVar],
-        _bit_vec: &[BoolVar],
-        _agg_vk: VerKeyVar,
+        vks: &[VerKeyVar],
+        bit_vec: &[BoolVar],
+        agg_vk: VerKeyVar,
     ) -> Result<(), CircuitError> {
-        todo!()
+        if vks.len() != bit_vec.len() {
+            return Err(CircuitError::ParameterError(format!(
+                "bit vector len {} != the number of stake keys {}",
+                bit_vec.len(),
+                vks.len(),
+            )));
+        }
+        let mut expect_agg_point = self.neutral_point_variable();
+        for (vk, &bit) in vks.iter().zip(bit_vec.iter()) {
+            // TODO: make API public in Jellyfish
+            let point =
+                self.binary_point_vars_select(bit, &self.neutral_point_variable(), &vk.0)?;
+            expect_agg_point = self.ecc_add::<P>(&expect_agg_point, &point)?;
+        }
+        self.enforce_point_equal(&expect_agg_point, &agg_vk.0)
     }
 
     fn check_stake_table_digest(
         &mut self,
-        _vks: &[VerKeyVar],
-        _stake_amts: &[Variable],
-        _digest: Variable,
+        vks: &[VerKeyVar],
+        stake_amts: &[Variable],
+        digest: Variable,
     ) -> Result<(), CircuitError> {
-        todo!()
+        if stake_amts.len() != vks.len() {
+            return Err(CircuitError::ParameterError(format!(
+                "the number of stake amounts {} != the number of stake verification keys {}",
+                stake_amts.len(),
+                vks.len(),
+            )));
+        }
+        let mut hash_input = vec![];
+        for (vk, &stake_amt) in vks.iter().zip(stake_amts.iter()) {
+            hash_input.push(vk.0.get_x());
+            hash_input.push(vk.0.get_y());
+            hash_input.push(stake_amt);
+        }
+        let expected_digest =
+            RescueNativeGadget::<F>::rescue_sponge_with_padding(self, &hash_input, 1)?[0];
+        self.enforce_equal(expected_digest, digest)
     }
 
     fn check_threshold(
