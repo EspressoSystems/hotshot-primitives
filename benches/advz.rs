05daec171fc70feca9e90ddd1e0e8d6b8804f177
Gus Gutoski <ggutoski@users.noreply.github.com>
2023-06-01 15:18:22 -0400

    address comments from https://github.com/EspressoSystems/hotshot-primitives/pull/47#pullrequestreview-1456019988

--- benches/advz.rs
+++ benches/advz.rs
@@ -1,5 +1,6 @@
 #![deny(warnings)]
 use ark_bls12_381::Bls12_381;
+use ark_bn254::Bn254;
 use ark_ec::pairing::Pairing;
 use ark_serialize::Write;
 use ark_std::rand::RngCore;
@@ -10,38 +11,43 @@ use hotshot_primitives::vid::{advz::Advz, VidScheme};
 use jf_primitives::pcs::{prelude::UnivariateKzgPCS, PolynomialCommitmentScheme};
 use sha2::Sha256;
 
-fn advz<E, H>(c: &mut Criterion)
+fn advz<E, H>(c: &mut Criterion, pairing_name: &str)
 where
     E: Pairing,
     // TODO(Gus) clean up nasty trait bounds upstream
     H: Digest + DynDigest + Default + Clone + Write,
     <<H as OutputSizeUser>::OutputSize as ArrayLength<u8>>::ArrayType: Copy,
 {
-    // (payload_chunk_size, num_storage_nodes)
-    let vid_sizes = [(20, 50), (40, 100), (60, 150), (80, 200), (100, 250)];
+    const RATE: usize = 4; // ratio of num_storage_nodes : polynomial_degree
+    const VID_SIZES_STEP: usize = 50;
+    const VID_SIZES_COUNT: usize = 4;
+
+    // (polynomial_degree, num_storage_nodes)
+    let vid_sizes: Vec<(usize, usize)> = (1..=VID_SIZES_COUNT)
+        .map(|x| (x * VID_SIZES_STEP, x * VID_SIZES_STEP * RATE))
+        .collect();
+    let supported_degree = vid_sizes.iter().max_by_key(|v| v.0).unwrap().0;
 
     // payload byte length
-    let byte_lens = [1000];
+    let byte_lens = [10_000, 100_000, 1_000_000];
 
     let mut rng = jf_utils::test_rng();
-    let srs = UnivariateKzgPCS::<E>::gen_srs_for_testing(
-        &mut rng,
-        vid_sizes.iter().max_by_key(|v| v.0).unwrap().0,
-    )
-    .unwrap();
+    let srs = UnivariateKzgPCS::<E>::gen_srs_for_testing(&mut rng, supported_degree).unwrap();
 
     // separate bench for each vid_sizes and byte_lens
-    for (payload_chunk_size, num_storage_nodes) in vid_sizes {
-        let advz = Advz::<E, H>::new(payload_chunk_size, num_storage_nodes, &srs).unwrap();
+    for len in byte_lens {
+        let mut bytes_random = vec![0u8; len];
+        rng.fill_bytes(&mut bytes_random);
 
-        for len in byte_lens {
-            let mut bytes_random = vec![0u8; len];
-            rng.fill_bytes(&mut bytes_random);
-
-            println!("advz_disperse_{}_{}", payload_chunk_size, num_storage_nodes);
+        for &(polynomial_degree, num_storage_nodes) in vid_sizes.iter() {
+            let advz = Advz::<E, H>::new(polynomial_degree, num_storage_nodes, &srs).unwrap();
 
             c.bench_function(
-                format!("advz_disperse_{}_{}", payload_chunk_size, num_storage_nodes).as_str(),
+                format!(
+                    "advz_disperse_{}_{}_{}",
+                    pairing_name, len, num_storage_nodes
+                )
+                .as_str(),
                 |b| b.iter(|| advz.dispersal_data(&bytes_random).unwrap()),
             );
         }
@@ -49,7 +55,8 @@ where
 }
 
 fn advz_main(c: &mut Criterion) {
-    advz::<Bls12_381, Sha256>(c);
+    advz::<Bls12_381, Sha256>(c, "Bls381");
+    advz::<Bn254, Sha256>(c, "Bn254");
 }
 
 criterion_group!(benches, advz_main);
