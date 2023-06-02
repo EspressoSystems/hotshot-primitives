52b2f713eea897778eb8a73f401c9b1b4a2dde19
Gus Gutoski <ggutoski@users.noreply.github.com>
2023-06-02 14:59:10 -0400

    feat: enable bench reports (#54)
    * enable bench reports
    
    * tweak comments as per https://github.com/EspressoSystems/hotshot-primitives/pull/54#pullrequestreview-1458185334

--- benches/advz.rs
+++ benches/advz.rs
@@ -1,15 +1,17 @@
-#![deny(warnings)]
+// #![deny(warnings)]
 use ark_bls12_381::Bls12_381;
 use ark_bn254::Bn254;
 use ark_ec::pairing::Pairing;
 use ark_serialize::Write;
 use ark_std::rand::RngCore;
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 use digest::{Digest, DynDigest, OutputSizeUser};
 use generic_array::ArrayLength;
 use hotshot_primitives::vid::{advz::Advz, VidScheme};
 use jf_primitives::pcs::{prelude::UnivariateKzgPCS, PolynomialCommitmentScheme};
 use sha2::Sha256;
+
+const KB: usize = 1 << 10;
 
 fn advz<E, H>(c: &mut Criterion, pairing_name: &str)
 where
@@ -18,39 +20,35 @@ where
     H: Digest + DynDigest + Default + Clone + Write,
     <<H as OutputSizeUser>::OutputSize as ArrayLength<u8>>::ArrayType: Copy,
 {
+    // play with these items
     const RATE: usize = 4; // ratio of num_storage_nodes : polynomial_degree
-    const VID_SIZES_STEP: usize = 50;
-    const VID_SIZES_COUNT: usize = 4;
+    let poly_degrees = [60, 80, 100, 120, 140];
+    let payload_byte_lens = [10 * KB, 100 * KB, 1000 * KB];
 
-    // (polynomial_degree, num_storage_nodes)
-    let vid_sizes: Vec<(usize, usize)> = (1..=VID_SIZES_COUNT)
-        .map(|x| (x * VID_SIZES_STEP, x * VID_SIZES_STEP * RATE))
-        .collect();
-    let supported_degree = vid_sizes.iter().max_by_key(|v| v.0).unwrap().0;
-
-    // payload byte length
-    let byte_lens = [10_000, 100_000, 1_000_000];
-
+    // more items as a function of the above
+    let supported_degree = *poly_degrees.iter().max().unwrap();
     let mut rng = jf_utils::test_rng();
     let srs = UnivariateKzgPCS::<E>::gen_srs_for_testing(&mut rng, supported_degree).unwrap();
 
-    // separate bench for each vid_sizes and byte_lens
-    for len in byte_lens {
+    for len in payload_byte_lens {
         let mut bytes_random = vec![0u8; len];
         rng.fill_bytes(&mut bytes_random);
 
-        for &(polynomial_degree, num_storage_nodes) in vid_sizes.iter() {
-            let advz = Advz::<E, H>::new(polynomial_degree, num_storage_nodes, &srs).unwrap();
+        let mut grp = c.benchmark_group(format!("advz_disperse_{}_{}KB", pairing_name, len >> 10));
+        grp.throughput(Throughput::Bytes(len as u64));
 
-            c.bench_function(
-                format!(
-                    "advz_disperse_{}_{}_{}",
-                    pairing_name, len, num_storage_nodes
-                )
-                .as_str(),
-                |b| b.iter(|| advz.dispersal_data(&bytes_random).unwrap()),
+        for &poly_degree in poly_degrees.iter() {
+            let num_storage_nodes = poly_degree * RATE;
+            let advz = Advz::<E, H>::new(poly_degree, num_storage_nodes, &srs).unwrap();
+            grp.bench_with_input(
+                BenchmarkId::from_parameter(num_storage_nodes),
+                &num_storage_nodes,
+                |b, _| {
+                    b.iter(|| advz.dispersal_data(&bytes_random).unwrap());
+                },
             );
         }
+        grp.finish();
     }
 }
 
