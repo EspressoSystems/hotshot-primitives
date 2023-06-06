21b6adbcbfb6510dafefd722bd8fd1d0f2638094
Charles Chen <chancharles92@gmail.com>
2023-06-06 16:28:49 -0400

    integrate nonnative-ecc apis

--- src/circuit/qc_keyagg.rs
+++ src/circuit/qc_keyagg.rs
@@ -1,41 +1,49 @@
-//! Circuit implementation of stake key aggregation for quorum certificates.
+//! Circuit implementation of stake key aggregation for quorum certificates verification.
 
+use ark_ec::twisted_edwards::Affine;
 use ark_ec::twisted_edwards::TECurveConfig as Config;
+use ark_ff::PrimeField;
 use ark_std::{format, vec};
 use jf_primitives::{circuit::rescue::RescueNativeGadget, rescue::RescueParameter};
 use jf_relation::{
-    errors::CircuitError, gadgets::ecc::PointVariable, BoolVar, Circuit, PlonkCircuit, Variable,
+    errors::CircuitError,
+    gadgets::{
+        ecc::{non_native::EmulatedPointVariable, Point},
+        EmulationConfig,
+    },
+    BoolVar, Circuit, PlonkCircuit, Variable,
 };
 
 #[derive(Debug, Clone)]
 /// Stake public key variable
-/// TODO: use non-native point variable
-pub struct VerKeyVar(pub PointVariable);
+/// Wrap EmulatedPointVariable because we need to simulate curve operations over the same curve's scalar field
+pub struct VerKeyVar<E: PrimeField>(pub EmulatedPointVariable<E>);
 
 /// Plonk circuit gadget for stake key aggregation for quorum certificates.
-pub trait QCKeyAggregateGadget<F, P>
+pub trait QCKeyAggregateGadget<F>
 where
     F: RescueParameter,
-    P: Config<BaseField = F>,
 {
     /// Key aggregation circuit
     /// * `vks` - list of stake public keys.
     /// * `bit_vec` - the indicator vector for the quorum set, `bit_vec[i] = 1` if `i` is in the quorum set, o/w `bit_vec[i] = 0`.
     /// * `agg_vk` - the public aggregated stake key.
-    fn check_aggregate_vk(
+    /// * `d_ecc` - the twisted Edward curve parameter for the simulated curve
+    fn check_aggregate_vk<E: EmulationConfig<F>, P: Config<BaseField = E>>(
         &mut self,
-        vks: &[VerKeyVar],
+        vks: &[VerKeyVar<E>],
         bit_vec: &[BoolVar],
-        agg_vk: VerKeyVar,
+        agg_vk: &VerKeyVar<E>,
+        d_ecc: E,
     ) -> Result<(), CircuitError>;
 
     /// Stake table commitment checking circuit
     /// * `vk` - list of stake public keys.
     /// * `stake_amts` - list of stake amounts for the corresponding stake keys.
     /// * `digest` - the hash of the stake table.
-    fn check_stake_table_digest(
+    fn check_stake_table_digest<E: EmulationConfig<F>>(
         &mut self,
-        vks: &[VerKeyVar],
+        vks: &[VerKeyVar<E>],
         stake_amts: &[Variable],
         digest: Variable,
     ) -> Result<(), CircuitError>;
@@ -52,16 +60,16 @@ where
     ) -> Result<(), CircuitError>;
 }
 
-impl<F, P> QCKeyAggregateGadget<F, P> for PlonkCircuit<F>
+impl<F> QCKeyAggregateGadget<F> for PlonkCircuit<F>
 where
     F: RescueParameter,
-    P: Config<BaseField = F>,
 {
-    fn check_aggregate_vk(
+    fn check_aggregate_vk<E: EmulationConfig<F>, P: Config<BaseField = E>>(
         &mut self,
-        vks: &[VerKeyVar],
+        vks: &[VerKeyVar<E>],
         bit_vec: &[BoolVar],
-        agg_vk: VerKeyVar,
+        agg_vk: &VerKeyVar<E>,
+        d_ecc: E,
     ) -> Result<(), CircuitError> {
         if vks.len() != bit_vec.len() {
             return Err(CircuitError::ParameterError(format!(
@@ -70,19 +78,22 @@ where
                 vks.len(),
             )));
         }
-        let mut expect_agg_point = self.neutral_point_variable();
+        let neutral_point = Point::from(Affine::<P>::zero());
+        let emulated_neutral_point_var =
+            self.create_constant_emulated_point_variable(neutral_point)?;
+        let mut expect_agg_point_var = emulated_neutral_point_var.clone();
         for (vk, &bit) in vks.iter().zip(bit_vec.iter()) {
-            // TODO: make API public in Jellyfish
-            let point =
-                self.binary_point_vars_select(bit, &self.neutral_point_variable(), &vk.0)?;
-            expect_agg_point = self.ecc_add::<P>(&expect_agg_point, &point)?;
+            let point_var =
+                self.binary_emulated_point_vars_select(bit, &emulated_neutral_point_var, &vk.0)?;
+            expect_agg_point_var =
+                self.emulated_ecc_add::<E>(&expect_agg_point_var, &point_var, d_ecc)?;
         }
-        self.enforce_point_equal(&expect_agg_point, &agg_vk.0)
+        self.enforce_emulated_point_equal(&expect_agg_point_var, &agg_vk.0)
     }
 
-    fn check_stake_table_digest(
+    fn check_stake_table_digest<E: EmulationConfig<F>>(
         &mut self,
-        vks: &[VerKeyVar],
+        vks: &[VerKeyVar<E>],
         stake_amts: &[Variable],
         digest: Variable,
     ) -> Result<(), CircuitError> {
@@ -95,8 +106,8 @@ where
         }
         let mut hash_input = vec![];
         for (vk, &stake_amt) in vks.iter().zip(stake_amts.iter()) {
-            hash_input.push(vk.0.get_x());
-            hash_input.push(vk.0.get_y());
+            hash_input.append(&mut vk.0 .0.to_vec());
+            hash_input.append(&mut vk.0 .1.to_vec());
             hash_input.push(stake_amt);
         }
         let expected_digest =
