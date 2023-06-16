fd27bd23843328c8a6f15c13a5ebf354b483f6e9
chancharles92 <chancharles92@users.noreply.github.com>
2023-06-16 14:17:33 +0000

    deploy: 0a94f5f75f37596e58f131efe8e2dcb957799ee0

---
+++ hotshot_primitives/circuit/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":["qc_keyagg"]};
--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"hotshot_primitives":["",[["stake_table",[],["config.rs","error.rs","mod.rs","utils.rs"]],["vdf",[],["minroot.rs","mod.rs"]],["vid",[],["advz.rs","mod.rs"]]],["lib.rs","quorum_certificate.rs"]]\
+"hotshot_primitives":["",[["circuit",[],["mod.rs","qc_keyagg.rs"]],["stake_table",[],["config.rs","error.rs","mod.rs","utils.rs"]],["vdf",[],["minroot.rs","mod.rs"]],["vid",[],["advz.rs","mod.rs"]]],["lib.rs","quorum_certificate.rs"]]\
 }');
 createSourceSidebar();
