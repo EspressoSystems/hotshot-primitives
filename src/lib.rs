0a94f5f75f37596e58f131efe8e2dcb957799ee0
chancharles92 <chancharles92@gmail.com>
2023-06-16 10:14:21 -0400

    SNARK circuit for key aggregation in QC verification (#45)
    * init trait
    
    * check_threshold circuit
    
    * draft circuits for keyagg and digest_hash
    
    * change jellyfish dependency
    
    * integrate nonnative-ecc apis
    
    * add test
    
    * debug
    
    * minor
    
    * fix test
    
    * fix clippy
    
    * change dependency back

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-#![deny(warnings)]
 // #![warn(missing_docs)] // TODO need rustdoc for stake_table
 
+pub mod circuit;
 pub mod quorum_certificate;
 pub mod stake_table;
 pub mod vdf;
